56869313c4b53a37a17ef198aecd12a3ba35f5b2
sdk-team <sdk-team@alibabacloud.com>
2020-04-28 17:13:34 +0800

    Generated csharp SDK for Bss-2014-07-14.

---
+++ bss-20140714/csharp/core/Models/QueryForCssOrderRequest.cs
@@ -0,0 +1,18 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Bss20140714.Models
+{
+    public class QueryForCssOrderRequest : TeaModel {
+        [NameInMap("paramStr")]
+        [Validation(Required=true)]
+        public string ParamStr { get; set; }
+
+    }
+
+}
