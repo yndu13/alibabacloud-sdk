eea290abf100180999ff1d4b684028d4d199692e
sdk-team <sdk-team@alibabacloud.com>
2020-06-02 17:31:07 +0800

    Generated csharp 2019-12-30 for facebody.

---
+++ facebody-20191230/csharp/core/Models/EnhanceFaceRequest.cs
@@ -0,0 +1,18 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.facebody20191230.Models
+{
+    public class EnhanceFaceRequest : TeaModel {
+        [NameInMap("ImageURL")]
+        [Validation(Required=true)]
+        public string ImageURL { get; set; }
+
+    }
+
+}
---
+++ facebody-20191230/csharp/core/Models/ListFaceDbsRequest.cs
@@ -0,0 +1,14 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.facebody20191230.Models
+{
+    public class ListFaceDbsRequest : TeaModel {
+    }
+
+}
