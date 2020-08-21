3709a344850a84f9991e87049bdbc35a35397396
sdk-team <sdk-team@alibabacloud.com>
2020-08-21 06:00:43 +0000

    Generated csharp 2018-07-13 for Ft.

--- ft-20180713/csharp/core/Properties/AssemblyInfo.cs
+++ ft-20180713/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0f633b70-e303-11ea-a9aa-654b09667918")]
+[assembly: Guid("94ff87a0-e373-11ea-b5a0-674229f3e8b5")]
 
 // Version information for an assembly consists of the following four values:
 //
